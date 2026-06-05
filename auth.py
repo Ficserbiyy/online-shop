a7f9a7159d11a26442385f1fec3338ed80e775e2
ficserbiYY <ficserbiy@gmail.com>
2026-06-05 18:22:57 +0500

    feat: implement User registragion and logout endpoints

---
+++ auth.py
@@ -0,0 +1,89 @@
+from fastapi import APIRouter, Depends, HTTPException, status, Response, Request
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlmodel.ext.asyncio.session import AsyncSession
+from models import User, UserCreate, settings
+from passLogic import verify_password, create_access_token, hash_password, decode_access_token
+from dbLogic import get_session
+from sqlmodel import select
+
+
+router = APIRouter(prefix="/auth", tags=["Authentication"])
+
+
+
+
+async def get_user_by_email(session: AsyncSession, email: str) -> User | None:
+    ''' To get user by email '''
+    statement = select(User).where(User.email == email)
+    result = await session.execute(statement)
+    return result.scalars().first()
+
+
+async def get_current_user(request: Request, session: AsyncSession = Depends(get_session)) -> User:
+    ''' To get current user '''
+    token = request.cookies.get("shopping_session")
+    
+    if not token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Not Authorized",
+        )
+    email = decode_access_token(token)
+    user = await get_user_by_email(session, email)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User Not Found",
+        )
+    return user
+
+
+
+@router.post("/register", status_code=201)
+async def register(user_data: UserCreate, session: AsyncSession = Depends(get_session)):
+    ''' Registration '''
+    existing_user = await get_user_by_email(session, user_data.email)
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email registered")
+    
+    hashed = hash_password(user_data.password)
+    db_user = User(email=user_data.email, hashed_password=hashed, is_active=True)
+    
+    session.add(db_user)
+    await session.commit()
+    return {"detail": "Successfully registered"}
+
+
+
+@router.post("/login")
+async def login(
+    response: Response,
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    session: AsyncSession = Depends(get_session)
+):
+    ''' Login '''
+    user = await get_user_by_email(session, form_data.username)
+    if not user or not verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email address or password"
+        )
+        
+    access_token = create_access_token(data={"sub": user.email})
+    response.set_cookie(
+        key="shopping_session",
+        value=access_token,               # JWT token
+        httponly=True,     
+        max_age=settings.JWT_EXPIRE * 60, # Cookie TTL
+        secure=False, 
+        samesite="lax" 
+    )
+    return {"detail": "Successfully logged in"}
+
+
+
+@router.post("/logout")
+async def logout(response: Response):
+    ''' Logout '''
+    response.delete_cookie(key="shopping_session")
+    return {"detail": "Successfully logged out"}
