afa3549acd732951dd1750a252467dbfd9731c98
ficserbiYY <ficserbiy@gmail.com>
2026-06-07 16:14:55 +0500

    test: update test_orders due to endpoint "create order" update.

--- tests/test_orders.py
+++ tests/test_orders.py
@@ -15,24 +15,46 @@ async def test_create_order_workflow():
         login_response = await ac.post("/auth/login", data={"username": user_data["email"], "password": user_data["password"]})
         assert login_response.status_code == 200
 
-        item_data = {
+        # Item №1
+        item1_res = await ac.post("/products/", json={
             "name": "Sublime Text 3",
             "description": "Thing that can't be bought",
-            "price": 4.99,
+            "price": 80.0,
+            "stock_quantity": 5
+        })
+        id1 = item1_res.json()["id"]
+
+        # Item №2
+        item2_res = await ac.post("/products/", json={
+            "name": "Coffee",
+            "description": "To code 8 hours a day",
+            "price": 15.0,
             "stock_quantity": 10
-        }
-        item_response = await ac.post("/products/", json=item_data)
-        assert item_response.status_code == 200
-        created_item = item_response.json()
-        item_id = created_item["id"]
-
-        order_response = await ac.post(f"/orders/?item_id={item_id}&quantity=3")
-        assert order_response.status_code == 201
-        assert order_response.json()["detail"] == "Order Successfully created"
-
-        products_response = await ac.get("/products/")
-        assert products_response.status_code == 200
+        })
+        id2 = item2_res.json()["id"]
         
-        products = products_response.json()
-        updated_item = next(i for i in products if i["id"] == item_id)
-        assert updated_item["stock_quantity"] == 7
+        
+        
+        order_payload = {
+            "items": [
+                {"item_id": id1, "quantity": 2},
+                {"item_id": id2, "quantity": 3}
+            ]
+        }
+        order_response = await ac.post("/orders/", json=order_payload)
+        
+        assert order_response.status_code == 201
+        res_data = order_response.json()
+        assert res_data["detail"] == "Order Successfully created", "Creation Failed"
+        
+        # (80 * 2) + (15 * 3) = 205:
+        assert res_data["total_cost"] == 205.0
+
+        products_res = await ac.get("/products/")
+        products = products_res.json()
+        
+        updated_item1 = next(i for i in products if i["id"] == id1)
+        updated_item2 = next(i for i in products if i["id"] == id2)
+        
+        assert updated_item1["stock_quantity"] == 3 # 5 - 2 = 3
+        assert updated_item2["stock_quantity"] == 7 # 10 - 3 = 7
